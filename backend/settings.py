64ca9617943f7270030652c883ad98f0f7c6c62a
Ramamoorthy <raamamoorthy@gmail.com>
2024-11-09 00:31:28 +0530

    Static file fix

--- backend/settings.py
+++ backend/settings.py
@@ -136,8 +136,11 @@ USE_I18N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
-STATIC_URL = 'static/'
-
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # or os.path.join(BASE_DIR, "static") depending on your Django version
+]
+STATIC_ROOT = BASE_DIR / "staticfiles" 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
